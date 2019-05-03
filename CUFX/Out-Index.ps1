e77a9dab552ce5724dcbbaa17e92d8c54202d7bf
dlacroix01 <36041312+dlacroix01@users.noreply.github.com>
2019-05-03 09:23:17 -0600

    update ensures correct browser pagination

--- CUFX/Out-Index.ps1
+++ CUFX/Out-Index.ps1
@@ -1,6 +1,3 @@
-﻿<#
-.Synopsis
-Generates the index.html file in any subdirectories named "HTML".
 .Example
 .\Out-Index.ps1
 Wrote C:\Projects\CUFX\trunk\CUFX\Generator\bin\Debug\GeneratedFiles\HTML\index.html
@@ -12,11 +9,11 @@ if(!$HtmlFolders) { Write-Error "No HTML folders found. Make sure you run the ge
 else { write "$HtmlFolders" }
 $HtmlFolders |% {
     pushd $_
-    gc -raw $PSScriptRoot\index.prefix |Out-File -enc utf8 index.html
+    gc -raw $PSScriptRoot\index.prefix |Out-File -enc utf8 index-iframe.html
     ls *.html |
         % {"<li><a href='$([IO.Path]::GetFileName($_.FullName))'>$([IO.Path]::GetFileNameWithoutExtension($_.FullName))</a></li>"} |
-        Out-File -enc utf8 index.html -Append
-    Out-File -enc utf8 index.html -InputObject "</ul></body></html>"
+        Out-File -enc utf8 index-iframe.html -Append
+    Out-File -enc utf8 index-iframe.html -InputObject "</ul></body></html>" -Append
     write "Wrote $PWD\index.html"
     popd
 }
